6446618a0d24e5a29430ed272a824edd407ba4a0
unknown <alanj.guerrero@gmail.com>
2024-06-30 15:11:46 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+// index.js
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const pool = require('./db');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// Ruta para obtener datos de sensores
+app.get('/api/sensores', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM sensores');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// Ruta para añadir datos de sensores
+app.post('/api/sensores', async (req, res) => {
+  try {
+    const { tds, ph, oxigeno } = req.body;
+    const newSensor = await pool.query(
+      'INSERT INTO sensores (tds, ph, oxigeno) VALUES($1, $2, $3) RETURNING *',
+      [tds, ph, oxigeno]
+    );
+    res.json(newSensor.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
